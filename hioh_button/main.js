96b0fd48b18de3aec590326340a61cda3b55a7b8
Vincent Barletta <vb3472@pleasantonusd.net>
2018-02-20 22:04:39 +0000

    Create personal website

---
+++ hioh_button/main.js
@@ -0,0 +1,12 @@
+function sayThatWasEasy() {
+  var thatWasEasy = new Audio("hioh.mp3");
+  thatWasEasy.play();
+}
+$("#easy").on("click", sayThatWasEasy);
+
+$(document).keypress(delegateKeypress);
+function delegateKeypress(event) {
+  if (event.charCode == 32) {
+    $("#easy").trigger("click");
+  }
+}
